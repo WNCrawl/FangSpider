4c93b6e2b21a60e7364c34aeacefde930290e42d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 23:17:40 +0000

    Adding tarfile member sanitization to extractall()

--- scrapydweb/projects/deploy.py
+++ scrapydweb/projects/deploy.py
@@ -248,7 +248,26 @@ class UploadView(MyView):
                     f.extractall(tmpdir)
         else:  # tar.gz
             with tarfile.open(filepath, 'r') as f:  # Open for reading with transparent compression (recommended).
-                f.extractall(tmpdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, tmpdir)
                 f.close()
 
         self.logger.debug("Uncompress to %s" % tmpdir)
